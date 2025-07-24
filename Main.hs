74eaa1ffa4e37959919fb62ba815269b86b7b614
Euler <eulergomesrocha@gmail.com>
2025-07-24 08:33:33 -0300

    Adds basic menu structure

---
+++ Main.hs
@@ -0,0 +1,31 @@
+module Main where
+    
+import System.Exit
+
+
+
+data Task = Task String deriving(Show)
+
+
+
+menu:: [Task]->IO()
+menu task = do
+    putStrLn "1 - Adicionar tarefa"
+    putStrLn "2 - Remover tarefa"
+    putStrLn "3 - Exibir tarefa"
+    putStrLn "4 - Sair"
+    putStrLn " Escolha uma opção:"
+    op <- getLine
+    case op of
+        "1" -> do
+            putStrLn"1"
+        "2" -> do
+            putStrLn"2"
+        "3" -> do 
+            putStrLn"3"
+        "4" -> do       
+             exitWith ExitSuccess
+
+main::IO()
+main = do
+    menu[]
