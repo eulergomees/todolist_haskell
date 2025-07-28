b054f895c18f2b11a00ff5c8b914d1776c0615ab
Euler <eulergomesrocha@gmail.com>
2025-07-28 14:10:14 -0300

    Adds task removal and display functionality

--- Main.hs
+++ Main.hs
@@ -3,7 +3,7 @@ module Main where
 import System.Exit
 
 
-data Tarefa = Tarefa String deriving(Show)
+newtype Tarefa = Tarefa String deriving(Show)
 
 adicionarTarefa :: [Tarefa] -> IO [Tarefa]
 adicionarTarefa tarefas = do
@@ -12,11 +12,35 @@ adicionarTarefa tarefas = do
     let novaTarefa = Tarefa desc
     return (tarefas ++ [novaTarefa])
 
+removerTarefa :: [Tarefa] -> IO [Tarefa]
+removerTarefa [] = do
+    putStrLn "Nenhuma tarefa para remover."
+    return []
+removerTarefa tarefas = do
+    putStrLn "Qual tarefa deseja remover ?"
+    mapM_ (\(i, Tarefa desc) -> putStrLn (show i ++ ". " ++ desc)) (zip [1..] tarefas)
+    entrada <- getLine
+    let indice = read entrada :: Int
+    if indice < 1 || indice > length tarefas
+        then do
+            putStrLn "Não existe essa tarefa!"
+            return tarefas
+        else do
+            let (antes, _:depois) = splitAt (indice - 1) tarefas
+            putStrLn "Tarefa removida!"
+            return (antes ++ depois)
+
+
+exibirTarefas :: [Tarefa] -> IO ()
+exibirTarefas [] = putStrLn "Nenhuma tarefa cadastrada."
+exibirTarefas tarefas = do
+    putStrLn "Lista de tarefas:"
+    mapM_ (\(i, Tarefa desc) -> putStrLn (show i ++ ". " ++ desc)) (zip [1..] tarefas)
 
 
 menu :: [Tarefa] -> IO ()
 menu tarefas = do
-    putStrLn "---Menu---"
+    putStrLn" "
     putStrLn "1. Adicionar tarefa"
     putStrLn "2. Remover tarefa"
     putStrLn "3. Exibir tarefas"
@@ -28,9 +52,11 @@ menu tarefas = do
             novasTarefas <- adicionarTarefa tarefas
             menu novasTarefas
         "2" -> do
-            putStrLn"2"
+            novasTarefas <- removerTarefa tarefas
+            menu novasTarefas
         "3" -> do
-            putStrLn"3"
+            exibirTarefas tarefas
+            menu tarefas
         "4" -> do
             putStrLn "Saindo do programa"
             exitSuccess
